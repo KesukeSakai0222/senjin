b8e58d55eddf24d172330a7230d5ec288f0811df
Kesuke Sakai <s1610285@edu.cc.uec.ac.jp>
2024-05-24 00:37:02 +0900

    partytown追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,16 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
+import partytown from "@astrojs/partytown";
 
 export default defineConfig({
-  integrations: [tailwind(),icon()]
+  integrations: [
+    tailwind(),
+    icon(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ]
 });
